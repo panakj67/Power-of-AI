4e7d0959ed0fbf0b07bcfb0ff811e4252d8055a0
Gourav Patel <kp180284@gmail.com>
2025-06-06 16:27:54 +0530

    added db

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import products from '../data/products.js';
 import Product from '../models/productModel.js';
 
-// .env
+// This is for .env
 dotenv.config();
 
 const connectDB = async () => {
